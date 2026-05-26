715f29803112753ab69d2442bc88cac91537e211
Maeve Clarke <263674179+dev-lizard@users.noreply.github.com>
2026-05-26 10:19:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+require([
+    "esri/Map",
+    "esri/views/MapView"
+  ], function(Map, MapView) {
+    const map = new Map({
+      basemap: "topo-vector"
+    });
+  
+    const view = new MapView({
+      container: "viewDiv",
+      map: map,
+      zoom: 4,
+      center: [15, 65] // longitude, latitude
+    });
+  });
+  
